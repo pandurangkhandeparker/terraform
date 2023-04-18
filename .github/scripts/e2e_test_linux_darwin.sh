fea53abb1ea94299ad1d3002a8d0d78ace1c2e71
Pandurang K <pandurang_k@persistent.com>
2023-04-18 15:46:21 +0530

    update s390x support (testing)

--- .github/scripts/e2e_test_linux_darwin.sh
+++ .github/scripts/e2e_test_linux_darwin.sh
@@ -8,13 +8,14 @@ then
     unzip -d $DIR "./terraform_${version}_${os}_${arch}.zip"
     sudo chmod +x $DIR/e2etest
     docker run --platform=linux/arm64 -v $DIR:/src -w /src arm64v8/alpine ./e2etest -test.v
-# elif [[ $arch == 's390x' ]] 
-# then
-#     export DIR=$(mktemp -d)
-#     unzip -d $DIR "${e2e_cache_path}/terraform-e2etest_${os}_${arch}.zip"
-#     unzip -d $DIR "./terraform_${version}_${os}_${arch}.zip"
-#     sudo chmod +x $DIR/e2etest
-#     docker run --platform=linux/s390x -v $DIR:/src -w /src s390x/alpine ./e2etest -test.v
+elif [[ $arch == 's390x' ]] 
+then
+    export DIR=$(mktemp -d)
+    unzip -d $DIR "${e2e_cache_path}/terraform-e2etest_${os}_${arch}.zip"
+    unzip -d $DIR "./terraform_${version}_${os}_${arch}.zip"
+    sudo chmod +x $DIR/e2etest
+    TF_ACC=1
+    docker run --platform=linux/s390x -v $DIR:/src -w /src s390x/alpine ./e2etest -test.v
 else
     unzip "${e2e_cache_path}/terraform-e2etest_${os}_${arch}.zip"
     unzip "./terraform_${version}_${os}_${arch}.zip"
